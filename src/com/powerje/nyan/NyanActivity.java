6c4339c427bab99801374af074a29e667bf151b6
James Power <heathen79@gmail.com>
2011-10-27 22:56:37 -0700

    Added settings to the App screen

--- src/com/powerje/nyan/NyanActivity.java
+++ src/com/powerje/nyan/NyanActivity.java
@@ -1,11 +1,17 @@
 package com.powerje.nyan;
 
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NyanActivity extends Activity {
 	private NyanView mRoot = null;
@@ -18,6 +24,7 @@ public class NyanActivity extends Activity {
 
 	@Override
 	public void onStart() {
+		super.onStart();
 		// scale by whichever is larger, width or height
 		DisplayMetrics metrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -27,7 +34,11 @@ public class NyanActivity extends Activity {
 
 		mRoot = new NyanView(this, largest);
 		setContentView(mRoot);
-		super.onStart();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
 		mPlayer = MediaPlayer.create(this, R.raw.dyan_loop);
 		mPlayer.setLooping(true);
 		mPlayer.setScreenOnWhilePlaying(true);
@@ -47,6 +58,21 @@ public class NyanActivity extends Activity {
 		});
 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			startActivity(new Intent(NyanActivity.this, NyanSettings.class));
+			break;
+		}
+		return true;
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
 	@Override
 	public void onStop() {
 		super.onStop();
@@ -59,7 +85,7 @@ public class NyanActivity extends Activity {
 		super.onPause();
 		mPlayer.pause();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
