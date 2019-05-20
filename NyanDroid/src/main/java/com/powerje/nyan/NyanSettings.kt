2e11b6921805b72348d931a9c11bd238e320e188
James Power <jep@lepoer.us>
2019-05-19 21:16:40 -0400

    Preferences actually work now. Who would've thought order mattered.

--- NyanDroid/src/main/java/com/powerje/nyan/NyanSettings.kt
+++ NyanDroid/src/main/java/com/powerje/nyan/NyanSettings.kt
@@ -1,9 +1,7 @@
 package com.powerje.nyan
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class NyanSettingsActivity : AppCompatActivity() {
@@ -23,26 +21,11 @@ class NyanSettingsActivity : AppCompatActivity() {
 
 }
 
-class NyanSettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+class NyanSettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.nyan_settings, rootKey)
         preferenceManager.sharedPreferencesName = getString(R.string.shared_preferences_name)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-
-    }
-
-    override fun onPause() {
-        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
-        super.onPause()
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences,
-                                           key: String) {
+        setPreferencesFromResource(R.xml.nyan_settings, rootKey)
     }
 
 }
