e7d74419752f15286f238e493129a39d854bd711
James Power <jep@lepoer.us>
2019-05-25 21:40:50 -0400

    Optimized the imports

--- NyanDroid/src/main/java/com/powerje/nyan/NyanPaper.kt
+++ NyanDroid/src/main/java/com/powerje/nyan/NyanPaper.kt
@@ -3,7 +3,6 @@ package com.powerje.nyan
 import android.content.SharedPreferences
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.os.AsyncTask
 import android.os.Handler
 import android.service.wallpaper.WallpaperService
 import android.util.Log
--- NyanDroid/src/main/java/com/powerje/nyan/NyanView.kt
+++ NyanDroid/src/main/java/com/powerje/nyan/NyanView.kt
@@ -6,7 +6,6 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
 import androidx.core.content.ContextCompat
