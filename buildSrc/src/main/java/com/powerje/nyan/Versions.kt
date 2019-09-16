d3411a44097c6e99198f96e2b1e512fa81a10c55
James Power <jep@lepoer.us>
2019-09-15 20:36:52 -0400

    Migrate gradle to Kotlin

---
+++ buildSrc/src/main/java/com/powerje/nyan/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+
+    object Build {
+        const val compileSdkVersion = 28
+        const val minSdkVersion = 19
+        const val targetSdkVersion = Versions.Build.compileSdkVersion
+    }
+
+}
